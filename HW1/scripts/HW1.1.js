241eb9c53edfc890307cb00109083d0381f220df
User <doronyuk714@mail.com>
2024-04-29 23:20:12 +0300

    complete HW1 without bonus task

---
+++ HW1/scripts/HW1.1.js
@@ -0,0 +1,17 @@
+let hello= 'hello';
+let owu='owu';
+let com='com';
+let ua='ua';
+let e= 1;
+let f= 10;
+let g= -999;
+let h= 123;
+let i= 3.14;
+let j= 2.7;
+let k= 16;
+let l= true;
+let m= false;
+
+console.log(hello,owu,com,ua,e,f,g,h,i,j,k,l,m);
+
+
---
+++ HW1/scripts/HW1.2.js
@@ -0,0 +1,8 @@
+let firstName = 'Doronyuk';
+let middleName ='Andriy';
+let lastName = 'Lyubomyrovych';
+let space = ' ';
+
+let person = firstName + space + middleName + space + lastName;
+
+console.log(person);
