8695ad7b7f3f888383b93af51dac00dd0c239826
User <doronyuk714@mail.com>
2024-05-09 07:36:13 +0300

    3 tasks before last less

--- Lesson4/scripts/HW1.js
+++ Lesson4/scripts/HW1.js
@@ -1,42 +1,57 @@
 /*- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б*/
 function areasq(a, b){
-   let result1 = a*b;
-    document.write(`square area: ${result1}</br>`);
+    if (a > 0 && b > 0) {
+        let result1 = a * b;
+        document.write(`square area: ${result1} if sides have lenght ${a} and ${b}</br>`);
+    }
+    else{
+        document.write(`uncorrect</br>`)
+    }
 }
 areasq(5, 8);
 
 /*- створити функцію яка обчислює та повертає площу кола з радіусом r*/
 function areacircle(p, r){
-    let result2 = p*r*r;
-    document.write(`circle area: ${result2}</br>`);
-}
-areacircle(3.14, 6);
+    if (p > 0 && r > 0) {
+        let result2 = p * r * r;
+        document.write(`circle area: ${result2} if radius have ${r}</br>`);
+    }
+    else{
+        document.write(`uncorrect</br>`)
+    }
+    }
+areacircle(3.14, 5);
 
 
 /*- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r*/
 function areacylind(p, r, h){
-    let result3 = 2*p*r*h;
-    document.write(`cylindr area: ${result3}</br>`);
+    if (p > 0 && r > 0 && h > 0) {
+        let result3 = 2 * p * r * h;
+        document.write(`cylindr area: ${result3} if radius = ${r} and height = ${h}</br>`);
+    }
+    else{
+        document.write(`uncorrect</br>`)
+    }
 }
 areacylind(3.14, 6, 10);
 
 
 
 /*- створити функцію яка приймає масив та виводить кожен його елемент*/
-function loop(arrayt4){
-    for (const item of arrayt4) {
+function loop(array1){
+    for (const item of array1) {
         document.write(`${item}, `);
     }
 }
-let array = [12, 'task4', 442, false];
+let array1 = [12, 'task4', 442, false];
 
-loop(array)
+loop(array1)
 /*- створити функцію яка створює параграф з текстом. Текст задати через аргумент*/
 function p(a){
     document.write(`<p>${a}</p>`);
 }
-p('forExample')
-p('andAgain')
+p('forExample');
+p('andAgain');
 
 
 /*- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий*/
@@ -44,10 +59,10 @@ document.write(`<ul>`)
 function li1(a1){
     document.write(`<li>${a1}</li>`);
 }
-li1('item')
-li1('item')
-li1('item')
-document.write(`</ul>`)
+li1('item');
+li1('item');
+li1('item');
+document.write(`</ul>`);
 
 
 /*- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)*/
@@ -55,36 +70,58 @@ document.write(`<ul>`)
 for (let i = 0; i < 3; i++){
     function li2(a2){
         document.write(`<li>${a2}</li>`);
-
     }
-    li2('item2')
+    li2('item2');
 }
-
-document.write(`</ul>`)
+document.write(`</ul>`);
 
 
 
 /*- створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список*/
 document.write(`<ul>`)
-function li3(){
-
-    document.write(`<li>${a}</li>`);
+function loop2(array2){
+    for (const item of array2) {
+        document.write(`<li>${item}</li> `);
+    }
 }
-li3('item')
-li3('item')
-li3('item')
+let array2 = [12, 'task4', 442, false];
+
+loop2(array2);
 document.write(`</ul>`)
 
 
 
 
 /*- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.*/
+function loop3(users) {
+    for (const user of users) {
+        document.write(`<div class="user">
+        <div class="id">id: ${user.id}</div>
+        <div class="name">name: ${user.name}</div>
+        <div class="age">age: ${user.age}</div>
+        </div>`);
+    }
+}
+let users = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false},
+    {id: 5, name: 'max', age: 30, status: true},
+    {id: 6, name: 'anya', age: 31, status: false},
+    {id: 7, name: 'oleg', age: 28, status: false},
+    {id: 8, name: 'andrey', age: 29, status: true},
+    {id: 9, name: 'masha', age: 30, status: true},
+    {id: 10, name: 'olya', age: 31, status: false},
+    {id: 11, name: 'max', age: 31, status: true}];
 
-
-
+loop3 (users);
 /*- створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13*/
-
-
+function sumArr(a,...rest) {
+    let summ = a + rest;
+    document.write(`summ of array = ${summ}</br>`);
+}
+sumArr(5, 8);
 
 /*- створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]*/
@@ -93,3 +130,14 @@ document.write(`</ul>`)
 
 /*- Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250*/
+function exchange(sumUa, USDCurrency, EURCurrency){
+    if (sumUa > 0 && USDCurrency > 0 && EURCurrency > 0) {
+        let sumUsd = sumUa / USDCurrency;
+        let sumEur= sumUa / EURCurrency;
+        document.write(`${sumUa} UAN can be exchanged for ${sumUsd} USD or ${sumEur} EUR</br>`);
+    }
+    else{
+        document.write(`uncorrect</br>`);
+    }
+}
+exchange(100000, 40, 42);
