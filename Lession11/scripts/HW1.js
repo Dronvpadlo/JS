260f34994c244f212e5e3f520c66f04a6861e031
User <doronyuk714@mail.com>
2024-05-30 10:06:22 +0300

    Complete 2 of 3 tasks from 11 lesson

--- Lession11/scripts/HW1.js
+++ Lession11/scripts/HW1.js
@@ -1,16 +1,16 @@
 //1.Взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
-let cartsBlock = document.getElementById('cartsBlock')
+let cartsBlock = document.getElementById('cartsBlock');
 fetch('https://dummyjson.com/carts')
     .then(res => res.json())
     .then(({carts}) =>{
         for (const cart of carts) {
             for (const item of cart.products) {
-                let cartInfo = document.createElement('div')
-                let id = document.createElement('div')
-                let title = document.createElement('div')
-                let price = document.createElement('div')
-                let quantity = document.createElement('div')
-                let total = document.createElement('div')
+                let cartInfo = document.createElement('div');
+                let id = document.createElement('div');
+                let title = document.createElement('div');
+                let price = document.createElement('div');
+                let quantity = document.createElement('div');
+                let total = document.createElement('div');
                 cartInfo.classList.add('cartInfo')
                 id.classList.add('cart');
                 id.innerText = `id: ${item.id}`
@@ -23,7 +23,7 @@ fetch('https://dummyjson.com/carts')
                 total.classList.add('cart');
                 total.innerText = `total: ${item.total}`
                 cartInfo.append(id, title, price, quantity, total);
-                cartsBlock.appendChild(cartInfo)
+                cartsBlock.appendChild(cartInfo);
             }
         }
     });
@@ -31,21 +31,27 @@ fetch('https://dummyjson.com/carts')
 
 
 
+
+
+
+
 //2.Взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
-let recipesBlock = document.getElementById('recipesBlock')
+let recipesBlock = document.getElementById('recipesBlock');
 fetch('https://dummyjson.com/recipes')
     .then(res => res.json())
     .then(({recipes}) => {
         for (const recipe of recipes) {
-            console.log(recipe)
+            let recipeInfo =document.createElement('div');
             let recipeBlock = document.createElement('div');
+            let recipeName = document.createElement('div');
             let instructions = document.createElement('div');
             let ingredientsList = document.createElement('ul');
-            let recipeImg = document.createElement('img')
+            let recipeImg = document.createElement('img');
             recipeBlock.classList.add('recipe');
-
-            recipeBlock.innerText = `name: ${recipe.name},
-                id: ${recipe.id},
+            recipeName.innerText = `name: ${recipe.name}`
+            recipeName.classList.add('recipeName');
+            recipeInfo.classList.add('recipeInfo')
+            recipeBlock.innerText = `id: ${recipe.id},
                 calories per serving: ${recipe.caloriesPerServing},
                 cook time minutes: ${recipe.cookTimeMinutes},
                 cuisine: ${recipe.cuisine},
@@ -57,22 +63,49 @@ fetch('https://dummyjson.com/recipes')
 
             recipeImg.src = `${recipe.image}`
             for (const item of recipe.instructions){
-                let instruction = document.createElement('p')
+                let instruction = document.createElement('p');
                 instruction.innerText =`${item}`
-                instructions.appendChild(instruction)
+                instructions.appendChild(instruction);
             }
             for (const item of recipe.ingredients) {
                 let ingredient = document.createElement('li');
                 ingredient.innerText = `${item}`
-                ingredientsList.append(ingredient)
-
+                ingredientsList.appendChild(ingredient);
             }
-            recipesBlock.append(recipeBlock, recipeImg, ingredientsList, instructions)
+            recipeInfo.append(recipeName, recipeBlock, recipeImg, ingredientsList, instructions);
+            recipesBlock.appendChild(recipeInfo);
         }
     });
+
+
+
+
+
+
 
 /*3. зробити файл users.html
 з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
     вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
 при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
 отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   де ХХХ - айді користувача)*/
+let usersBlock = document.getElementById('usersBlock');
+let baseUrs = 'http://jsonplaceholder.typicode.com/'
+let getUsersUrl = baseUrs + '/users'
+let getUserById = (id)=> getUsersUrl + '/' + id
+fetch(getUsersUrl)
+    .then((response) => response.json())
+    .then((users) => {
+        for (const user of users) {
+            let userList = document.createElement('ul');
+            let userId = document.createElement('li');
+            let userName = document.createElement('li');
+            let userHref = document.createElement('a');
+            userList.classList.add('userInfo')
+            userHref.href = getUserById(user.id);
+            userHref.innerText = getUsersUrl + `${user.id}`
+            userId.innerText = `Id: ${user.id}`
+            userName.innerText = `Name: ${user.name}`
+            userList.append(userId, userName, userHref);
+            usersBlock.appendChild(userList);
+        }
+    })
